5e16a43ff2cff75780d669bcdd395f7f459e5f3b
Infinidrix <biruksolomon11@gmail.com>
2021-02-04 11:27:35 +0300

    add starter code

---
+++ Lesson 04 [Lab 06]/Starter/assets/js/bom.js
@@ -0,0 +1,96 @@
+/*  
+Here is the exercise on working on the remaining bom method 
+
+Location , Navigator , screen , Window 
+
+Follow the Instruction on the comments 
+
+1. Declare the UI Variables for selecting on the elements 
+2. Use the innerHTML property to display the result on each element 
+3. The Text  of the elements will lead you what bom information is required 
+
+Adding Extra is Possible if you want to explore more ...
+
+Good Luck !!! 
+*/
+
+
+
+
+// Define UI Variables  here 
+
+var canvas = document.querySelector("#c");
+var gl = canvas.getContext("webgl");
+if (!gl){
+    console.log("No webgl supported.")
+}
+var vertexShaderSource = document.querySelector("#vertex-shader-2d").text;
+var fragmentShaderSource = document.querySelector("#fragment-shader-2d").text;
+
+var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+var program = createProgram(gl, vertexShader, fragmentShader);
+var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+var positionBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+// three 2d points
+var positions = [
+    0, 0,
+    0, 0.5,
+    0.7, 0,
+  ];
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+// webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+gl.clearColor(0, 0, 0, 0);
+gl.clear(gl.COLOR_BUFFER_BIT);
+// Tell it to use our program (pair of shaders)
+gl.useProgram(program);
+gl.enableVertexAttribArray(positionAttributeLocation);
+// Bind the position buffer.
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+// Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+var size = 2;          // 2 components per iteration
+var type = gl.FLOAT;   // the data is 32bit floats
+var normalize = false; // don't normalize the data
+var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+var offset = 0;        // start at the beginning of the buffer
+gl.vertexAttribPointer(
+    positionAttributeLocation, size, type, normalize, stride, offset)
+var primitiveType = gl.TRIANGLES;
+var offset = 0;
+var count = 3;
+gl.drawArrays(primitiveType, offset, count);
+function createShader(gl, type, source) {
+    var shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (success) {
+      return shader;
+    }
+  
+    console.log(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+  }
+
+  function createProgram(gl, vertexShader, fragmentShader) {
+    var program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (success) {
+      return program;
+    }
+  
+    console.log(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+  }
+
+
+
+
+
+// Display the BOM Information on the innerHTML of the elements
